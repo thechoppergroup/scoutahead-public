3508bb40c7cb4cdd33958484f23345e0563be418
Adrian_Miller <adrian.miller@build.com>
2015-03-31 20:24:39 -0700

    Added gemfile, updated menu and built out app js

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var app = angular.module( 'app', [
+	'ngRoute',
+	'ui.router',
+	'app.controllers'
+	], function($interpolateProvider) {
+	  $interpolateProvider.startSymbol('[[');
+	  $interpolateProvider.endSymbol(']]');
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+	$stateProvider
+
+	// route to show our basic form (/form)
+	.state('page',{
+		url: '/*path',
+		controller: 'homeCtrl',
+		templateUrl: function ($stateParams){
+			if($stateParams.path) {
+				return $stateParams.path;
+			} else {
+				return '/pages/index.html'
+			}
+  	}
+	})
+	$urlRouterProvider.otherwise('/');
+})
---
+++ pages/contact.md
@@ -0,0 +1,13 @@
+---
+layout: page
+title: Contact
+permalink: contact
+order: 4
+---
+
+
+<form action="//formspree.io/you@email.com">
+    <input type="text" name="name">
+    <input type="email" name="_replyto">
+    <input type="submit" value="Send">
+</form>
