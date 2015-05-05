69167bc9cdf0b079432d778c663e992f7696717c
Adrian Miller <adrianjonmiller@gmail.com>
2015-05-04 23:55:25 -0700

    fixed disqus plugin

--- js/controllers.js
+++ js/controllers.js
@@ -2,4 +2,16 @@ angular.module('app.controllers', [])
 
 	.controller('homeCtrl', function($scope, $rootScope) {
   })
+
+  .controller('disqusCtrl', function(){
+  	/* * * CONFIGURATION VARIABLES * * */
+    var disqus_shortname = '50wordsaday';
+    
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+  })
 ;
