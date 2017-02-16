252520669dc994d79a0833d123b5616b140717b3
Adrian Miller <adrianjonmiller@gmail.com>
2017-02-16 10:58:06 -0800

    new site updates

--- _assets/scripts/scripts.js
+++ _assets/scripts/scripts.js
@@ -1,3 +1,48 @@
+Js.Behaviors.video = function (container) {
+  console.log('video loaded')
+    // select video element
+  var vid = document.getElementById('vault');
+  var windowheight = $(window).height();
+
+
+  var scrollpos = window.pageYOffset;
+  var targetscrollpos = scrollpos;
+  var accel = 0;
+
+
+  // ---- Values you can tweak: ----
+  var accelamount = 0.01; //How fast the video will try to catch up with the target position. 1 = instantaneous, 0 = do nothing.
+  var bounceamount = 0; //value from 0 to 1 for how much backlash back and forth you want in the easing. 0 = no bounce whatsoever, 1 = lots and lots of bounce
+
+  // pause video on load
+  vid.pause();
+  window.onscroll = function(){
+    targetscrollpos = window.pageYOffset/400;
+  };
+
+
+  setInterval(function(){
+      accel += (targetscrollpos - scrollpos)*accelamount;
+
+      if (accel > 1) accel = 1;
+      if (accel < -1) accel = -1;
+
+      scrollpos = (scrollpos + accel) * (bounceamount) + (targetscrollpos * (1-bounceamount));
+
+      vid.currentTime = scrollpos;
+      vid.pause();
+
+  }, 40);
+}
+
+Js.Behaviors.shuttle = function (container) {
+  console.log(container)
+  window.addEventListener('scroll', function () {
+    container.style.top = window.pageYOffset + 'px';
+  })
+}
+
+
 Js.Behaviors.load = function(container){
   var hash = window.location.hash;
   var state = hash.slice(1, window.location.hash.length);
@@ -98,29 +143,29 @@ Js.Behaviors.faqScroll = function(container) {
   })
 }
 
-Js.Behaviors.welcomeLink = function(container){
-  function act() {
-     $(container).on('click', function(e){
-       e.preventDefault();
-
-       if(userIsLoggedIn) {
-         window.location.href = "/?noredirect";
-       } else {
-         window.location.href = "/"
-       }
-     });
-
-  }
-
-  if (_.isUndefined(window.userIsLoggedIn)) {
-    xhr.insecurePost("/login", {}, function (response) {
-       window.userIsLoggedIn = response.loggedIn;
-       act();
-    });
-  } else {
-    act();
-  }
-}
+// Js.Behaviors.welcomeLink = function(container){
+//   function act() {
+//      $(container).on('click', function(e){
+//        e.preventDefault();
+//
+//        if(userIsLoggedIn) {
+//          window.location.href = "/?noredirect";
+//        } else {
+//          window.location.href = "/"
+//        }
+//      });
+//
+//   }
+//
+//   if (_.isUndefined(window.userIsLoggedIn)) {
+//     xhr.insecurePost("/login", {}, function (response) {
+//        window.userIsLoggedIn = response.loggedIn;
+//        act();
+//     });
+//   } else {
+//     act();
+//   }
+// }
 
 Js.Behaviors.facebookShare = function(container){
   var $this = $(container);
@@ -138,20 +183,20 @@ Js.Behaviors.twitterShare = function(container){
   })
 };
 
-Js.Behaviors.loggedInHide = function(container) {
-  if (_.isUndefined(window.userIsLoggedIn)) {
-    xhr.insecurePost("/login", {}, function (response) {
-      window.userIsLoggedIn = response.loggedIn;
-      if (userIsLoggedIn) {
-        $(container).addClass('is-hidden');
-      }
-    });
-  } else {
-     if (userIsLoggedIn) {
-       $(container).addClass('is-hidden');
-     }
-  }
-}
+// Js.Behaviors.loggedInHide = function(container) {
+//   if (_.isUndefined(window.userIsLoggedIn)) {
+//     xhr.insecurePost("/login", {}, function (response) {
+//       window.userIsLoggedIn = response.loggedIn;
+//       if (userIsLoggedIn) {
+//         $(container).addClass('is-hidden');
+//       }
+//     });
+//   } else {
+//      if (userIsLoggedIn) {
+//        $(container).addClass('is-hidden');
+//      }
+//   }
+// }
 
 Js.Behaviors.OlympicsTimer = function(container) {
   function zeroPad(x) {
@@ -178,29 +223,29 @@ Js.Behaviors.OlympicsTimer = function(container) {
 }
 
 
-Js.Behaviors.slick = function(container){
-  xhr.insecureGet('/surveys/weekly_polls/featured', function (response) {
-    d3.selectOne('.js-featured-polls').templated(response, function (poll, pollLi) {
-      pollLi.populateFrom(poll);
-      pollLi.selectOne('.js-poll_link_ref').attr("href", poll.poll_link);
-    });
-    if (response.length > 1 ) {
-	    $(container).slick({
-		    dots: true,
-		    infinite: true,
-		    speed: 300,
-		    slidesToShow: 1,
-		    centerMode: true,
-		    autoplay: true,
-		    autoplaySpeed: 4000,
-        dots: false,
-        arrows: false,
-        centerPadding: '0px'
-	    })
-    };
-  });
-  console.log("success");
-}
+// Js.Behaviors.slick = function(container){
+//   xhr.insecureGet('/surveys/weekly_polls/featured', function (response) {
+//     d3.selectOne('.js-featured-polls').templated(response, function (poll, pollLi) {
+//       pollLi.populateFrom(poll);
+//       pollLi.selectOne('.js-poll_link_ref').attr("href", poll.poll_link);
+//     });
+//     if (response.length > 1 ) {
+// 	    $(container).slick({
+// 		    dots: true,
+// 		    infinite: true,
+// 		    speed: 300,
+// 		    slidesToShow: 1,
+// 		    centerMode: true,
+// 		    autoplay: true,
+// 		    autoplaySpeed: 4000,
+//         dots: false,
+//         arrows: false,
+//         centerPadding: '0px'
+// 	    })
+//     };
+//   });
+//   console.log("success");
+// }
 
 Js.Behaviors.tagShow = function (container) {
   var tags = container.getAttribute('data-tags');
@@ -224,7 +269,7 @@ Js.Behaviors.replaceSection = function (container) {
   var queryParams = (container.getAttribute('data-query-params') || "").split(/\s*,\s*/);
   var url = container.getAttribute('data-snip-url');
   var started = false;
-  queryParams.forEach(function(param) { 
+  queryParams.forEach(function(param) {
       if (! param) return;
       var paramValue = queryParameter(param);
       if (!paramValue) return;
@@ -234,10 +279,51 @@ Js.Behaviors.replaceSection = function (container) {
       }
       url += param + '=' + paramValue; // should be subject to URL escaping if it comes up.
   });
-  
+
   section.replaceSection(null, url, function () {
     section.revealMain();
   }, function(){
     Js._init(section);
   });
+}
+
+Js.Behaviors.product = function (container) {
+  container.style.display = '0';
+  $(container).on('inview', function(event, inview){
+    if(inview) {
+      container.style.opacity = '1';
+    } else {
+      container.style.opacity = '0';
+    }
+  })
+}
+
+Js.Behaviors.scroll = function (container) {
+  var content = {
+    'intro': document.getElementById('content:intro'),
+    'product': document.getElementById('content:product'),
+    'beyond': document.getElementById('content:beyond')
+  };
+
+  window.addEventListener('scroll', function (e) {
+    console.log(e.currentTarget.pageYOffset)
+
+    if(e.currentTarget.pageYOffset < 1200) {
+      content.intro.style.opacity = '1'
+    } else {
+      content.intro.style.opacity = '0'
+    }
+
+    if(e.currentTarget.pageYOffset < 3600 && e.currentTarget.pageYOffset > 2400) {
+      content.product.style.opacity = '1'
+    } else {
+      content.product.style.opacity = '0'
+    }
+
+    if(e.currentTarget.pageYOffset < 4800 && e.currentTarget.pageYOffset > 3600) {
+      content.beyond.style.opacity = '1'
+    } else {
+      content.beyond.style.opacity = '0'
+    }
+  })
 }
